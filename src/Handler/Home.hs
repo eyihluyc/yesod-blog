a416aebc18b3c4a4d0f7ce3be74e5ef118e65a7f
eyihluyc <theviolthevioletandthewine@gmail.com>
2024-05-31 12:47:01 -0400

    Use `Esqueleto.Experimental` instead of `Esqueleto`

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -3,24 +3,27 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE BlockArguments #-}
 
 module Handler.Home where
-
-import Database.Esqueleto ((^.))
-import qualified Database.Esqueleto as E
+import qualified Database.Esqueleto.Experimental as E
 import Import
 
 getHomeR :: Handler Html
 getHomeR = do
     allPosts <- runDB $
-        E.select $
-            E.from $ \(blog_post `E.InnerJoin` author) -> do
-                E.on $ blog_post ^. BlogPostAuthorId E.==. author ^. AuthorId
-                return
-                    ( blog_post ^. BlogPostId
-                    , blog_post ^. BlogPostTitle
-                    , author ^. AuthorEmail
-                    , author ^. AuthorId
-                    )
+        E.select do
+            (blog_post E.:& author) <- E.from $ 
+                E.table @BlogPost
+                `E.InnerJoin` E.table @Author
+                `E.on` (\(blog_post E.:& author) -> 
+                    blog_post E.^. BlogPostAuthorId E.==. author E.^. AuthorId)
+            return 
+                ( blog_post E.^. BlogPostId
+                , blog_post E.^. BlogPostTitle
+                , author E.^. AuthorEmail
+                , author E.^. AuthorId
+                )
     defaultLayout $ do
         $(widgetFile "posts/index")
